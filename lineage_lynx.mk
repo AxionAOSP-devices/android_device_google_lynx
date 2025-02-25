917d12b8967b4c363a577ebcd098d94c59bb748b
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-02-25 08:58:07 +0800

    lynx: Ship viperfx
    Change-Id: I6264723c3ae9f0c52f489cb460c2130890eaeb1e

--- lineage_lynx.mk
+++ lineage_lynx.mk
@@ -29,6 +29,8 @@ AXION_CAMERA_REAR_INFO := 64,13
 AXION_CAMERA_FRONT_INFO := 13
 AXION_MAINTAINER := rmp
 
+TARGET_INCLUDE_VIPERFX := true
+
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="lynx-user 16 BP2A.250805.005 13691446 release-keys" \
     BuildFingerprint=google/lynx/lynx:16/BP2A.250805.005/13691446:user/release-keys \
